29fbee488bcf82062f58ac6734f4a1b318fb51a8
Zack Doll <zackrdoll@gmail.com>
2020-12-11 15:37:11 -0800

    completed puzzle 2!

--- Day2/Day2.java
+++ Day2/Day2.java
@@ -4,6 +4,16 @@ import java.io.File;
 import java.util.Scanner;
 
 public class Day2 {
+
+    /**
+     * The Toboggon shop needs help fixing their passwords...
+     * Each line in the file pointed to by the given scanners if of the format
+     * <low_bound>:<upper_bound> <rule_char>: <password_string>, where the rule char
+     * must occur in the password_string between some amount within the bounds (inclusive).
+     * 
+     * @param input_scan
+     * @return Number of valid passwords found
+     */
     public static int ValidPasswords(Scanner input_scan) {
         int num_valid = 0; // count of valid passwords
 
@@ -39,12 +49,59 @@ public class Day2 {
         return num_valid;
     }
 
+
+    /**
+     * Those were the wrong rules! The format is the same as specified previously,
+     * but were <low_bound> and <upper_bound> are actually <index_1> and <index_2>,
+     * where exactly one of the the characters at these indexes (1-indexed) must be
+     * equal to rule_char.
+     * 
+     * @param input_scan
+     * @return Number of valid passwords
+     */
+    public static int ValidPasswords2(Scanner input_scan) {
+        int num_valid = 0; // count of valid passwords
+
+        int index_1, index_2, curr_char_count; // used for counting occurrences
+        char rule_char; // used for given rule
+        String[] line; // given line in file, split by space
+        String curr_pass;
+
+        while (input_scan.hasNext()) {
+            line = input_scan.nextLine().split(" ");
+
+            index_1   = Integer.parseInt(String.valueOf(line[0].split("-")[0]));
+            index_2 = Integer.parseInt(String.valueOf(line[0].split("-")[1]));
+
+            rule_char       = line[1].charAt(0);
+            curr_char_count = 0;
+            curr_pass       = line[2];
+
+            if (curr_pass.charAt(index_1-1) == rule_char) {
+                curr_char_count++;
+            }
+
+            if (curr_pass.charAt(index_2-1) == rule_char) {
+                curr_char_count++;
+            }
+
+            if (curr_char_count == 1) {
+                num_valid++;
+            }
+        }
+
+        return num_valid;
+    }
+
     public static void main(String[] args) throws Exception {
         String file_path = "./Day2/input_day2.txt";
 
         try {
             Scanner input_scan = new Scanner(new File(file_path));
-            System.out.println("Valid passwords: " + ValidPasswords(input_scan));
+            System.out.println("Valid passwords (v1): " + ValidPasswords(input_scan));
+
+            input_scan = new Scanner(new File(file_path));
+            System.out.println("Valid passwords (v2): " + ValidPasswords2(input_scan));
         } catch (Exception e) {
             e.printStackTrace();
         }
