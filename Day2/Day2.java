e0a346fd09f78dc7bda3ae1dc30cf85e07be5029
Zack Doll <zackrdoll@gmail.com>
2020-12-11 14:46:38 -0800

    ValidPasswords skeleton

--- Day2/Day2.java
+++ Day2/Day2.java
@@ -1,9 +1,25 @@
 package Day2;
 
-public class Day2 {
-    
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-        
+public class Day2 {
+    public static int ValidPasswords(Scanner input_scan) {
+        int num_valid = 0;
+
+
+        return num_valid;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+    
+        try {
+            Scanner input_scan = new Scanner(new File(args[0]));
+
+        } catch (Exception e) {
+            System.out.println("Problem opening file: ");
+            e.printStackTrace();
+        }
     }
 }
