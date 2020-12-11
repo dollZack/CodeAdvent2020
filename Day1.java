34c30a3f94a15b252121827786927050c07b9f93
Zack Doll <zackrdoll@gmail.com>
2020-12-11 14:19:36 -0800

    finished Day 1! :)

--- Day1.java
+++ Day1.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.HashSet;
 import java.util.Scanner;
+import java.lang.Math;
 
 public class Day1 {
     /**
@@ -17,6 +18,7 @@ public class Day1 {
         the 1010 possible sums to 2020, exiting once we find the sum. 
         */
 
+        // Populate HashSet
         HashSet<Integer> report_entries = new HashSet<Integer>();
         int product = -1;
         int line;
@@ -25,12 +27,89 @@ public class Day1 {
             report_entries.add(line);
         }
 
+        // Find the pair
         int num = 0;
         while (num <= 1010 && product == -1) {
             if (report_entries.contains(num) && report_entries.contains(2020-num)) {
                 product = num*(2020-num);
             }
             num++;
+        }
+
+        /* 
+        note: this could be optimized by starting with the minimum value in the entry,
+        and going to the 1010 or the max, whichever is smaller.
+        */
+
+        return product;
+    }
+
+
+    /** 
+     * Same as the previous repair, but now by finding three numbers which sum to 2020,
+     * rather than two.
+     * @param input_scan
+     * @return
+     */
+    public static int ReportRepair2(Scanner input_scan) {
+        int product = -1;
+        /* 
+        I wonder if this can be done similarly to the previous repair...
+        The file contains all positive entries.
+        Possible sums are 
+        1, 1, 2018
+        ...
+        1, 1009, 1010
+        2, 2, 2016
+        ...
+        2, 1008, 1010
+        2, 1009, 1009
+        2020-2 = 2018 -> 2018/2 = 1009
+
+        3, 3, 2014
+        ...
+        3, 1007, 1010
+        3, 1008, 1009
+
+        2020 - 3 = 2017 -> 2017/2 = 1008.5
+        Bound on when to stop for giving first integer is when (2020-i-j >= ceil((2020-i)/2)),
+        where i and j are the first and second integers
+
+        When to stop iterating i?
+        2020//3 = 673
+
+        673, 673, 674;
+        2020-673= 1347 -> ceil(1347/2) = 674
+
+        
+
+        */
+
+        // Populate HashSet;
+        HashSet<Integer> report_entries = new HashSet<Integer>();
+        int line;
+        while (input_scan.hasNext()) {
+            line = input_scan.nextInt();
+            report_entries.add(line);
+        }
+
+        // Find the 3-tuple
+        int i = 0;
+        int j, k;
+        while (i <= 673 && product == -1) {
+            j = i;
+            k = 2020-i-j;
+
+            while (k>=(Math.ceil((2020-i)/2)) && product == -1) {
+                if (report_entries.contains(i) && report_entries.contains(j) && report_entries.contains(k)) {
+                    product = i*j*k;
+                }
+
+                j++;
+                k--;
+            }
+
+            i++;
         }
 
         return product;
@@ -48,6 +127,10 @@ public class Day1 {
         try {
             Scanner input_scan = new Scanner(new File(args[0]));
             System.out.println("Repairs: " + ReportRepair(input_scan));
+
+            input_scan = new Scanner(new File(args[0]));
+            System.out.println("Repairs pt. 2: " + ReportRepair2(input_scan));
+            
         } catch (Exception e) {
             System.out.println("Error opening file:");
             e.printStackTrace();
