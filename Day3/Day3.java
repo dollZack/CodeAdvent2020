bedcf4208d0496234b02adb309c84176f685f542
Zack <zackrdoll@gmail.com>
2020-12-12 11:17:56 -0800

    finished day 3

--- Day3/Day3.java
+++ Day3/Day3.java
@@ -27,6 +27,14 @@ class Day3 {
         int tree_count = 0;
         int column     = 0;
 
+        // First line out of while b/c needed line_length
+        if (line.charAt(column) == '#') {
+            tree_count++;
+        }
+
+        column = (column+3)%line_length;
+
+        // Traverse rest of hill!
         while (input_scan.hasNextLine()) {
             line = input_scan.nextLine();
             if (line.charAt(column) == '#') {
@@ -38,14 +46,85 @@ class Day3 {
 
         return tree_count;
     }
+
+    /**
+     * Now traverse the hill with varying slopes, returning the product trees 
+     * encountered of these slopes
+     * @param file_name
+     * @return Product
+     * @throws Exception
+     */
+    public static long slopeComparison(String file_name) throws Exception{
+        long count_product = 1;
+        int[][] slopes    = {
+            {1,1}, {3,1}, {5,1}, {7,1}, {1,2}
+        };
+
+        for (int[] slope : slopes) {
+            int column_offset = slope[0];
+            int row_offset    = slope[1];
+
+            try {
+                Scanner input_scan = new Scanner(new File(file_name));
+
+                String line   = "";
+                int column    = 0;
+                int num_trees = 0;
+
+                if (!input_scan.hasNextLine()) {
+                    System.out.println("File pointed to by input_scan had no line entries..");
+                    System.exit(0);
+                } else {
+                    line = input_scan.nextLine();
+                }
+
+                if (line.charAt(column) == '#') {
+                    num_trees++;
+                }
+
+                column = (column+column_offset)%line.length();
+
+                // traverse rest of hill
+                while(input_scan.hasNextLine()) {
+                    int rows_viewed = 0;
+
+                    //go down row_offset lines
+                    while(input_scan.hasNextLine() && rows_viewed < row_offset) {
+                        line = input_scan.nextLine();
+                        rows_viewed++;
+                    }
+                    
+                    if (rows_viewed == row_offset) { // if not true, we have read through the entire file
+                        // tree?
+                        if (line.charAt(column) == '#') {
+                            num_trees++;
+                        }
+                    }
+
+                    // increment column
+                    column = (column+column_offset)%line.length();
+                }
+
+                // Finished hill with current slope
+                count_product*=num_trees;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.exit(1);
+            }
+        }
+
+        return count_product;
+    }
     
 
     public static void main(String[] args) throws Exception {
-        String file_name = "input_day3.txt";
+        String file_name = "./Day3/input_day3.txt";
         
         try {
             Scanner input_scan = new Scanner(new File(file_name));
             System.out.println("Trees encountered: " + countTrees(input_scan));
+            System.out.println("Tree count product: " + slopeComparison(file_name));
         } catch (Exception e) {
             e.printStackTrace();
         }
