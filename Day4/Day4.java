e3f1f444f1dfa2945760626fd55acbb23857ddbf
Zack Doll <zackrdoll@gmail.com>
2020-12-13 12:49:27 -0800

    working on puzzle 2, tweaked original solution to use HashMap

--- Day4/Day4.java
+++ Day4/Day4.java
@@ -1,10 +1,69 @@
 package Day4;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Scanner;
 
+import java.util.Iterator;
+
 public class Day4 {
+    static final HashSet<String>  valid_requirements = new HashSet<String>(Arrays.asList("byr","iyr","eyr","hgt","hcl","ecl","pid"));
+
+    /**
+     * Checks if the given credentials contain all of the required credentials,
+     * and that every field meets its particular rules.
+     * 
+     * @param curr_credentials
+     * @return True if the credentials are valid, else false.
+     */
+    public static Boolean validPassport (HashMap<String, String> curr_credentials) {
+        Iterator<String> required_creds = valid_requirements.iterator();
+        Boolean valid = curr_credentials.keySet().containsAll(valid_requirements);
+        while (valid && required_creds.hasNext()) {
+            String curr_cred  = required_creds.next();
+            // String curr_value = curr_credentials.get(curr_cred);
+
+            switch (curr_cred) {
+                case "byr":
+                    break;
+                
+                case "iyr":
+
+                    break;
+
+                case "eyr":
+
+                    break;
+
+                case "hgt":
+
+                    break;
+
+                case "hcl":
+
+                    break;
+
+                case "ecl":
+
+                    break;
+
+                case "pid":
+
+                    break;
+                
+                default:
+                    System.out.println("Unexpected key in required_credentials...");
+                    System.exit(0);
+                    break; // never runs
+            }
+        }
+
+        return valid;
+    }
+
+
     /**
      * The security is struggling to figure out which passports are valid!
      * A valid passport contains a byr, iyr, eyr, hgt, hcl, ecl, pid and cid.
@@ -15,27 +74,22 @@ public class Day4 {
      * This function reads through the file of passports and returns the number
      * which are valid, subject to our definition ;) Each field is space delimited,
      * of the form <field>:<value>, and each passport is seperated by a blank line.
+     * 
+     * Edit for puzzle 2: Furthermore, every field has a strict format, it must follow
+     * to be considered valid. Thus, a valid passport must contain every field, and every
+     * field must follow it's correct format.
      * @param input_scan
      * @return Number of 'valid' passports
      */
     public static int passportChecks(Scanner input_scan) {
-        HashSet<String> valid_requirements = new HashSet<String>();
-        valid_requirements.add("byr");
-        valid_requirements.add("iyr");
-        valid_requirements.add("eyr");
-        valid_requirements.add("hgt");
-        valid_requirements.add("hcl");
-        valid_requirements.add("ecl");
-        valid_requirements.add("pid");
-
         String[] curr_fields;
         String          line             = input_scan.hasNextLine() ? input_scan.nextLine() : null;
         int             valid_passports  = 0;
-        HashSet<String> curr_credentials = new HashSet<String>();
+        HashMap<String, String> curr_credentials = new HashMap<String, String>();
 
         while(line != null) {
             if (line.equals("")) {
-                if (curr_credentials.containsAll(valid_requirements)) {
+                if (curr_credentials.keySet().containsAll(valid_requirements)) {
                     valid_passports++;
                 }
                 
@@ -44,7 +98,8 @@ public class Day4 {
             } else {
                 curr_fields = line.split(" ");
                 for (String field : curr_fields) {
-                    curr_credentials.add(field.split(":")[0]);
+                    String[] key_value = field.split(":");
+                    curr_credentials.put(key_value[0], key_value[1]);
                 }
             }
 
@@ -52,7 +107,7 @@ public class Day4 {
         }
 
         // check current credentials at the end of the file!
-        if (curr_credentials.containsAll(valid_requirements)) {
+        if (curr_credentials.keySet().containsAll(valid_requirements)) {
             valid_passports++;
         }
 
