53d6c0a517d5fdc36fe52965ef873c391b14ed79
Zack <zackrdoll@gmail.com>
2020-12-13 09:19:18 -0800

    adds Day4 skeleton

---
+++ Day4/Day4.java
@@ -0,0 +1,28 @@
+package Day4;
+
+import java.util.Scanner;
+
+public class Day4 {
+    /**
+     * The security is struggling to figure out which passports are valid!
+     * A valid passport contains a byr, iyr, eyr, hgt, hcl, ecl, pid and cid.
+     * We're gonna help them identify valid passports more quickly. But b/c
+     * We only have North Pole credentials, which don't have cid's, we'll call
+     * those who are only missing cid's valid.
+     * 
+     * This function reads through the file of passports and returns the number
+     * which are valid, subject to our definition ;)
+     * @param input_scan
+     * @return
+     */
+    public static int passportChecks(Scanner input_scan) {
+        int valid_passports = 0;
+
+
+        return valid_passports;
+    }
+
+    public static void main(String[] args) throws Exception {
+        
+    }
+}
