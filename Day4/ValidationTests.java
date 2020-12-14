a71f699b1c20dd8964b09d72483bd687734c86ac
Zack Doll <zackrdoll@gmail.com>
2020-12-14 12:11:45 -0800

    adds unit tests file for day4 puzzle 2

---
+++ Day4/ValidationTests.java
@@ -0,0 +1,37 @@
+package Day4;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+/**
+ * ValidationTests
+ */
+class ValidationTests {
+    @Test void testByr() {
+
+    }
+
+    @Test void testIry() {
+
+    }
+
+    @Test void testEyr() {
+
+    }
+
+    @Test void testHgt() {
+
+    }
+
+    @Test void testHcl() {
+
+    }
+
+    @Test void testEcl() {
+        
+    }
+
+    @Test void testPid() {
+
+    }
+}
