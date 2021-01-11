15cbf8d0cdb42a7332bf94e0e7e3db8e26884e17
Zack Doll <zackrdoll@gmail.com>
2021-01-10 16:47:27 -0800

    finished day8

--- Day8/Day8.java
+++ Day8/Day8.java
@@ -84,12 +84,108 @@ public class Day8 {
         return accumulator;
     }
 
+    /**
+     * It seems a single instruction must be changed from jmp to nop, or from nop to jmp.
+     * Making the correct change will remove the infinite loop in the instructions!
+     * Find the instruction to change, and return the value of accumulator after execution of
+     * the final instruction.
+     * @param input_scan
+     * @return
+     */
+    private static int instructionCorrector(Scanner input_scan) {
+        /**
+         * We need to parse the instructions just like last time,
+         * here returning once we've executed the very last instruction.
+         * 
+         * As we iterate through the instructions, we can try adjust
+         * a found jmp/nop, and then try to execute until the final instruction.
+         * Like last time, we would know that we're in a loop by executing an instruction
+         * we've already seen before.
+         * So, if we iterate through, and find an instruction, we can change it, and then
+         * run this version of the instructions through a driver, which maintains history
+         * of instructions, and returns on duplicate (-1), or on final instruction (accum)
+         */
+        // parse file into arraylist
+        ArrayList<String> instruction_set = new ArrayList<String>();
+        while (input_scan.hasNextLine()) {
+            instruction_set.add(input_scan.nextLine());
+        }
+
+        int accumulator = -1; //return value
+        int instruction_index = 0;
+        while (accumulator < 0 && instruction_index < instruction_set.size()) {
+            String curr_instruction = instruction_set.get(instruction_index);
+            String[] instruction_array = curr_instruction.split(" ", 2);
+
+            if (instruction_array[0].equals("nop") || instruction_array[0].equals("jmp")) {
+                ArrayList<String> clone_set = (ArrayList<String>)instruction_set.clone();
+                instruction_array[0] = instruction_array[0].equals("nop") ? "jmp" : "nop";
+                clone_set.set(instruction_index, instruction_array[0] + " " + instruction_array[1]);
+
+                accumulator = correctorDriver(clone_set);
+            }
+
+            instruction_index++;
+        }
+
+        if (instruction_index == instruction_set.size()) {
+            System.out.println("ERROR: REACHED END OF INSTRUCTION SET");
+        }
+
+        return accumulator;
+    }
+
+
+    private static int correctorDriver(ArrayList<String> instructions) {
+        int accumulator = 0;
+        int index = 0;
+        HashSet<Integer> past_instructions = new HashSet<Integer>();
+
+        while (!past_instructions.contains(index) && !(index == instructions.size())) {
+            past_instructions.add(index);
+
+            String[] instruction_arr = instructions.get(index).split(" ");
+            // acc
+            if (instruction_arr[0].equals("acc")) {
+                int delta = Integer.valueOf(instruction_arr[1].substring(1));
+                if (instruction_arr[1].charAt(0) == '+') {
+                    accumulator+=delta;
+                } else {
+                    accumulator-=delta;
+                }
+
+                index++;
+            // jmp
+            } else if (instruction_arr[0].equals("jmp")) {
+                int delta = Integer.valueOf(instruction_arr[1].substring(1));
+                if (instruction_arr[1].charAt(0) == '+') {
+                    index+=delta;
+                } else {
+                    index-=delta;
+                }
+            // nop
+            } else {
+                index++;
+            }
+        }
+
+
+        if (index == instructions.size()) {
+            return accumulator;
+        } else {
+            return -1;
+        }
+    }
+
 
     public static void main(String[] args) {
         String file_path = "./Day8/day8_input.txt";
+        // String file_path = "./Day8/test_input.txt";
+
         try {
             Scanner input_scan = new Scanner(new File(file_path)); 
-            System.out.println("Accumulator before loop: " + handheldHalting(input_scan));
+            // System.out.println("Accumulator before loop: " + handheldHalting(input_scan));
+            System.out.println("Accumulator after final instruction: " + instructionCorrector(input_scan));
         } catch (Exception e) {
             e.printStackTrace();
         }
--- Makefile
+++ Makefile
@@ -2,9 +2,10 @@ all:
 	+$(MAKE) -C Day1
 	+$(MAKE) -C Day2
 	+$(MAKE) -C Day3
-	+$(MAKE) -C Day4
-	+$(MAKE) -C Day5
+	# +$(MAKE) -C Day4
+	# +$(MAKE) -C Day5
 	+$(MAKE) -C Day6
+	+$(MAKE) -C Day8
 
 clean:
 	rm -rf */*.class
