2d5c50a710fa3d2d82de643be4e9e614d0ad5006
Zack Doll <zackrdoll@gmail.com>
2021-03-16 10:53:34 -0700

    Create Day10 file

---
+++ Day10/Day10.java
@@ -0,0 +1,31 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Day10 {
+    
+    /**
+     * Your battery is dead, and the power outlet produces the wrong number of jolts.
+     * We must perform some foolery to with our own jolt adapters, to ensure that we can
+     * charge our device. 
+     * 
+     * Our device's built-in joltage adapter has a rating of 3 greater
+     * than our biggest joltage adapter, and a given joltage adapter can only connect
+     * to one that is 1-3 jolts greater than it. The outlet near us has a joltage of 0. 
+     * 
+     * We must find the sequence of connections between our adaptors, and keep track
+     * of the differences between every connection - storing the number of 1 jolt 
+     * differences and the number of 3 jolt differences, multiply these, and return.
+     * 
+     * @param input_scan A list of our joltage adapters.
+     */
+    private static void adapterArray(Scanner input_scan) {
+
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        String input_string = "";
+        // Scanner input_scan = new Scanner(new File(input_string));
+        System.out.println("Hello world!");
+    }
+}
