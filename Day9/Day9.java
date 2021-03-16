aa8bb700fce709e1309cf5af82fe173e42545a1a
Zack Doll <zackrdoll@gmail.com>
2021-03-16 09:31:19 -0700

    merging work on Day 9 from laptop

---
+++ Day9/Day9.java
@@ -0,0 +1,12 @@
+public class Day9 {
+    
+    public static void main(String[] args) {
+        String input_path = "./day9_input.txt";
+
+        try {
+            Scanner input_scan = new Scanner(new File(input_path));
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
